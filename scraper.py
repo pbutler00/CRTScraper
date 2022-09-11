b9e0ca1f5e445b9de11bc66c40fc74542f6c4dd6
Andrea Hayman <ahayman@hamilton.edu>
2022-09-11 16:56:54 -0400

    newspaper stuff

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from GoogleNews import GoogleNews
 from newspaper import Article
 from newspaper import Config
 import pandas
+import requests
 #config will allow us to access the specified url for which we are #not authorized. Sometimes we may get 403 client error while parsing #the link to download the article.
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
@@ -17,7 +18,15 @@ for article in result:
 print(links)
 
 for link in links:
-    article = Article(link)
-    article.download()
+    raw_html = requests.get(link)
+    article = Article('')
+    article.download(raw_html.content)
     article.parse()
+
+    print("Article's Title:")
+    print(article.title)
+    print("------------------")
+
+    print("Article's Text:")
     print(article.text)
+    print("------------------")
