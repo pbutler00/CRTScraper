1a1015cdb8c5e151f91db4fb212d3a17afdb04bb
Andrea Hayman <ahayman@hamilton.edu>
2022-09-26 17:14:40 -0400

    updated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,32 +1,133 @@
-from GoogleNews import GoogleNews
 from newspaper import Article
 from newspaper import Config
-import pandas
 import requests
-#config will allow us to access the specified url for which we are #not authorized. Sometimes we may get 403 client error while parsing #the link to download the article.
+from scrapingbee import ScrapingBeeClient
+import nltk
+import pandas as pd
+nltk.download('punkt')
 
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-config = Config()
-config.browser_user_agent = user_agent
-googlenews=GoogleNews(start='05/01/2020',end='05/31/2020')
-googlenews.search('Critical Race Theory')
-result=googlenews.result()
-links = []
-for article in result:
-        links.append(article['link'])
+our_api_key = "O8IZ2YUTDJ0TK9YV2LY7EAFMM0TB9ID6PGPVZOYA7JIGK33RILWEG28T2VRNBCVRVTV3499R9Z1OZ580"
+gn = GoogleNews()
+client = ScrapingBeeClient(api_key=our_api_key)
 
-print(links)
+news_data = pd.Dataframe(columns=['Title','Author','Date','Text','Summary','Keywords'])
 
-for link in links:
-    raw_html = requests.get(link)
-    article = Article('')
-    article.download(raw_html.content)
-    article.parse()
+wapo = gn.search('Critical Race Theory site:washingtonpost.com before:'+start_day'after:'+end_day)
+msnbc = gn.search('Critical Race Theory site:msnbc.com before:'+start_day'after:'+end_day)
+cnn = gn.search('Critical Race Theory site:cnn.com before:'+start_day'after:'+end_day)
+nyt = gn.search('Critical Race Theory site:nytimes.com before:'+start_day'after:'+end_day)
+fox = gn.search('Critical Race Theory site:foxnews.com before:'+start_day'after:'+end_day)
 
-    print("Article's Title:")
-    print(article.title)
-    print("------------------")
+def add_to_df(df, links):
+    for link in links:
+        raw_html = client.get(link["link"])
+        article = Article('')
+        article.download(raw_html.content)
+        article.parse()
+        article.nlp()
 
-    print("Article's Text:")
-    print(article.text)
-    print("------------------")
+        if article.title:
+            title = article.title
+        else:
+            title = 'None'
+        if article.authors:
+            authors = article.authors
+        else:
+            authors = 'None'
+        if article.publish_date:
+            date = article.publish_date
+        else:
+            date = 'None'
+        if article.text:
+            text = article.text
+        else:
+            text = 'None'
+        if article.summary:
+            summary = article.summary
+        else:
+            summary = 'None'
+        if article.keywords:
+            keywords = article.keywords
+        else:
+            keywords = 'None'
+
+        df = df.append({'Title':title,'Author':author,'Date':date,'Text':text,'Summary':summary,'Keywords':keywords})
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from GoogleNews import GoogleNews
+# from newspaper import Article
+# from newspaper import Config
+# # import pandas
+# import requests
+# #config will allow us to access the specified url for which we are #not authorized. Sometimes we may get 403 client error while parsing #the link to download the article.
+#
+# user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+# config = Config()
+# config.browser_user_agent = user_agent
+# googlenews=GoogleNews(start='05/01/2020',end='05/31/2020')
+# googlenews.search('Critical Race Theory')
+# result=googlenews.result()
+# links = []
+# for article in result:
+#         links.append(article['link'])
+#
+# print(links)
+#
+# for link in links:
+#     raw_html = requests.get(link)
+#     article = Article('')
+#     article.download(raw_html.content)
+#     article.parse()
+#
+#     print("Article's Title:")
+#     print(article.title)
+#     print("------------------")
+#
+#     print("Article's Text:")
+#     print(article.text)
+#     print("------------------")
--- tester.py
+++ tester.py
@@ -1,5 +1,7 @@
 import requests
 from newspaper import Article
+import nltk
+nltk.download('punkt')
 
 url = 'https://www.msnbc.com/rachel-maddow-show/maddowblog/mar-lago-case-doj-appeal-trumps-reaction-matters-rcna46983'
 raw_html= requests.get(url)
@@ -8,24 +10,31 @@ article.download(raw_html.content)
 
 article.parse()
 # nlp helps find article keywords -- isn't currently working
-# article.nlp()
+article.nlp()
 
 #To extract title
 print("Article's Title:")
 print(article.title)
 print("------------------")
 
-#To extract authors
-print("Article's Author(s):")
-print(article.authors)
+print("Article's Keywords:")
+print(article.keywords)
 print("------------------")
 
-#To extract text
-print("Article's Text:")
-print(article.text)
+print("Article's Summary:")
+print(article.summary)
 print("------------------")
-
-#To extract date
-print("Article's Publish Date:")
-print(article.publish_date)
-print("------------------")
+# #To extract authors
+# print("Article's Author(s):")
+# print(article.authors)
+# print("------------------")
+#
+# #To extract text
+# print("Article's Text:")
+# print(article.text)
+# print("------------------")
+#
+# #To extract date
+# print("Article's Publish Date:")
+# print(article.publish_date)
+# print("------------------")
